58fc04f8d14efd64500bb15a381a4241f90ddeed
Hardslog <hardslog@gmail.com>
2016-05-10 16:34:41 -0400

    Add new hardslog_dark theme
    - few fixes for the hardslog theme
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- java/src/com/android/inputmethod/keyboard/KeyboardTheme.java
+++ java/src/com/android/inputmethod/keyboard/KeyboardTheme.java
@@ -45,6 +45,8 @@ public final class KeyboardTheme implements Comparable<KeyboardTheme> {
     public static final int THEME_ID_AUTO_DARK = 5;
     public static final int THEME_ID_YOU = 6;
     public static final int DEFAULT_THEME_ID = THEME_ID_YOU;
+    public static final int THEME_ID_HS_DARK = 7;
+    public static final int DEFAULT_THEME_ID = THEME_ID_AUTO_DARK;
 
     private static KeyboardTheme[] AVAILABLE_KEYBOARD_THEMES;
 
@@ -66,6 +68,9 @@ public final class KeyboardTheme implements Comparable<KeyboardTheme> {
                 Build.VERSION_CODES.Q),
         new KeyboardTheme(THEME_ID_YOU, "You", R.style.KeyboardTheme_You,
                 Build.VERSION_CODES.S_V2),
+        new KeyboardTheme(THEME_ID_HS_DARK, "HSDark", R.style.KeyboardTheme_HS_Dark,
+                // This has never been selected as default theme.
+                VERSION_CODES.BASE),
     };
 
     static {
