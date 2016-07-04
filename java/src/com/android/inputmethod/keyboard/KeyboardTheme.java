0b65c2fade86197bd9b0069005b93ecb1b9fec04
Hardslog <hardslog@gmail.com>
2016-07-04 10:59:05 -0400

    New theme: BLACKOUT
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- java/src/com/android/inputmethod/keyboard/KeyboardTheme.java
+++ java/src/com/android/inputmethod/keyboard/KeyboardTheme.java
@@ -46,6 +46,7 @@ public final class KeyboardTheme implements Comparable<KeyboardTheme> {
     public static final int THEME_ID_YOU = 6;
     public static final int DEFAULT_THEME_ID = THEME_ID_YOU;
     public static final int THEME_ID_HS_DARK = 7;
+    public static final int THEME_ID_BLACKOUT = 8;
     public static final int DEFAULT_THEME_ID = THEME_ID_AUTO_DARK;
 
     private static KeyboardTheme[] AVAILABLE_KEYBOARD_THEMES;
@@ -69,6 +70,9 @@ public final class KeyboardTheme implements Comparable<KeyboardTheme> {
         new KeyboardTheme(THEME_ID_YOU, "You", R.style.KeyboardTheme_You,
                 Build.VERSION_CODES.S_V2),
         new KeyboardTheme(THEME_ID_HS_DARK, "HSDark", R.style.KeyboardTheme_HS_Dark,
+                // This has never been selected as default theme.
+                VERSION_CODES.BASE),
+        new KeyboardTheme(THEME_ID_BLACKOUT, "Blackout", R.style.KeyboardTheme_Blackout,
                 // This has never been selected as default theme.
                 VERSION_CODES.BASE),
     };
